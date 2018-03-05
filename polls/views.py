9e1bf68b8f2c133e83ae3f80aa932bb58a691400
ChaseKiner <chasekiner@hotmail.com>
2018-03-04 19:08:41 -0500

    Add vote

--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
 
-from .models import Question
+from .models import Question, Choice
 # Create your views here.
 
 
@@ -25,4 +25,16 @@ def results(request, question_id):
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/vote.html', {'question': question} )
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form
+           return render(request, 'polls/detail.html', {
+               'question': question,
+               'error_message': "You didn't select a choice.",
+               })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
