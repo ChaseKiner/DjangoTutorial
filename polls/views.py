e57fdb4c60f546a6d49193d3a470309e85f0c896
ChaseKiner <chasekiner@hotmail.com>
2018-03-06 15:31:01 -0500

    Fix formatting

--- polls/views.py
+++ polls/views.py
@@ -3,9 +3,6 @@ from django.http import HttpResponse
 from django.template import loader
 
 from .models import Question, Choice
-# Create your views here.
-
-
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -15,13 +12,16 @@ def index(request):
             }
     return render(request, 'polls/index.html', context)
 
+
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question} )
 
+
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question} )
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
